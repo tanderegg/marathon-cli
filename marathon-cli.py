57aa24f44920c767e40ed2df2d1c2ac937fb4936
Tim Anderegg <timothy.anderegg@gmail.com>
2017-01-03 08:31:56 -0500

    WIP: Add docs and handle TASK_FAILED.

--- marathon-cli.py
+++ marathon-cli.py
@@ -5,25 +5,32 @@ import time
 import requests
 import logging
 
-from marathon import MarathonClient, MarathonApp, MarathonHttpError, MarathonError
+from marathon import (MarathonClient, MarathonApp, MarathonHttpError,
+                      MarathonError)
 
+TASK_STATUS_URL = "{}/tasks.json"
 STDOUT_URL = "{}/files/read.json?path=/opt/mesos/slaves/{}/frameworks/{}/executors/{}/runs/{}/stdout"
 STDERR_URL = "{}/files/read.json?path=/opt/mesos/slaves/{}/frameworks/{}/executors/{}/runs/{}/stderr"
 OFFSET = "&offset={}&length={}"
 
 def get_task_by_version(client, app_id, version):
+    """
+    Gets the Mesos task using the Marathon version of the deployment.
+    """
     tasks = client.list_tasks(app_id=marathon_app_id)
     new_task = None
     for task in tasks:
-        # print task
         if task.version == version:
             new_task = task
     return new_task
 
 def print_file_chunk(url, offset, auth):
+    """
+    Takes a URL pointing to a Mesos file, and an offset, and prints
+    the file contents from offset to the end, then returns the new offset.
+    """
     length = requests.get(url, auth=auth, verify=False).json()['offset'] - offset
     offset_params = OFFSET.format(offset, length)
-    #stdout_url = STDOUT_URL_OFFSET_LENGTH.format(agent_hostname, new_task.slave_id, framework_id, new_task.id, container_id, stdout_offset, stdout_length)
     data = requests.get(url+offset_params, auth=auth, verify=False).json()['data']
     if data != "":
         for line in data.split('\n')[:-1]:
@@ -31,14 +38,36 @@ def print_file_chunk(url, offset, auth):
     return offset + length
 
 if __name__ == '__main__':
+    """
+    This script reads in values from environment variables, then deploys a
+    Marathon application as defined in MARATHON_APP.
+
+    MARATHON_URLS:              One or more URL's to try when communication with
+                                Marathon, separated by commas.
+    MARATHON_APP_ID:            The identifier of the application in Marathon.
+    MARATHON_USER:              The user to use, if needed.
+    MARATHON_PASSWORD:          The password to use, if needed.
+    MARATHON_FORCE:             Use the Force, if necessary (i.e. when a
+                                deployment is failing.)
+    MARATHON_FRAMEWORK_NAME:    The name of the framework (usually 'marathon')
+    MARATHON_APP:               The JSON formatted app definition.
+    MARATHON_RETRIES:           The number of task failures to tolerate.
+    MESOS_AGENT_MAP:            This is used when Mesos is behind a proxy.  The
+                                API will return the Mesos Agent IP address,
+                                but that may need to be mapped to a URL.  The
+                                map is defined like:
+                                10.0.1.34|https://mesos.test.dev,...
+    MESOS_MASTERS:              One or more urls for Mesos communication,
+                                separated by commas.
+    """
+
     marathon_urls = os.getenv("MARATHON_URLS", "http://localhost:8080").split(',')
     marathon_app_id = os.getenv("MARATHON_APP_ID", "test-app")
     marathon_user = os.getenv("MARATHON_USER", None)
     marathon_password = os.getenv("MARATHON_PASSWORD", None)
-    marathon_force = os.getenv("MARATHON_FORCE_DEPLOY", False)
+    marathon_force = True if os.getenv("MARATHON_FORCE_DEPLOY", "false") == "true" else False
     marathon_framework_name = os.getenv("MARATHON_FRAMEWORK_NAME", "marathon")
-    # This could be 10.0.1.34|https://mesos-agent-1.test.dev,10.0.1.35|https://mesos-agent-2.test.dev...
-    mesos_agent_map_string = os.getenv("MESOS_AGENT_MAP", None)
+    marathon_retries = os.getenv("MARATHON_RETRIES", 3)
     marathon_app = os.getenv("MARATHON_APP","""
         {
             "id": "/test-app",
@@ -62,12 +91,11 @@ if __name__ == '__main__':
                   "timeoutSeconds": 20,
                   "maxConsecutiveFailures": 3
                 }
-            ],
-            "env": {
-                "toggle": "true"
-            }
+            ]
         }
     """)
+    mesos_agent_map_string = os.getenv("MESOS_AGENT_MAP", None)
+    mesos_master_urls = os.getenv("MESOS_MASTER_URLS", "http://localhost:5050").split(',')
 
     exit_code = 0
     auth = None
@@ -77,46 +105,57 @@ if __name__ == '__main__':
     ### Setup Logging
     logging.basicConfig(level=logging.WARN)
 
-    print "Parsing JSON app definition..."
+    logging.info("Parsing JSON app definition...")
     app_definition = MarathonApp.from_json(json.loads(marathon_app))
 
     try:
-        print "Connecting to Marathon..."
+        logging.info("Connecting to Marathon...")
         client = MarathonClient(marathon_urls, username=marathon_user, password=marathon_password, verify=False)
     except MarathonError as e:
-        print "Failed to connect to Marathon! {}".format(e)
+        logging.error("Failed to connect to Marathon! {}".format(e))
         exit_code = 1
         sys.exit(exit_code)
 
-    print "Deploying application..."
+    logging.info("Deploying application...")
     try:
         app = client.get_app(marathon_app_id)
     except MarathonHttpError:
         response = client.create_app(marathon_app_id, app_definition)
+        version = response.version
+        depolyment_id = response.deployments[0].id
     else:
         response = client.update_app(marathon_app_id, app_definition, force=marathon_force)
+        version = response['version']
+        deployment_id = response['deploymentId']
 
-    print "New version deployed: ", response['version']
+    logging.info("New version deployed: {}".format(version))
 
     ### Get newly created Mesos task
 
     time.sleep(0.5)
-    deployments = client.get_app(marathon_app_id).deployments
-    new_task = get_task_by_version(client, marathon_app_id, response["version"])
-    deployment_id = response["deploymentId"]
+    #deployments = client.get_app(marathon_app_id).deployments
+    new_task = get_task_by_version(client, marathon_app_id, version)
 
     if not new_task:
-        print "New task did not start automatically, probably because the application definition did not change, forcing restart..."
+        logging.warn("New task did not start automatically, probably because the application definition did not change, forcing restart...")
         response = None
         for hostname in marathon_urls:
             try:
                 headers = {"content-type": "application/json"}
-                response = requests.post("{}/v2/apps/{}/restart".format(hostname, marathon_app_id), auth=auth, verify=False, headers=headers)
+                force_string = "true" if marathon_force else "false"
+                response = requests.post("{}/v2/apps/{}/restart?force={}".format(
+                    hostname, marathon_app_id, force_string), auth=auth, verify=False,
+                    headers=headers)
             except requests.exceptions.ConnectionError as e:
-                print "Marathon connection error, ignoring: {}".format(e)
+                logging.warn("Marathon connection error, ignoring: {}".format(e))
                 pass
             else:
                 break
+
+        if response.status_code != 200:
+            logging.error("Failed to force application restart, exiting...")
+            exit_code = 1
+            sys.exit(exit_code)
 
         time.sleep(2)
         new_task = get_task_by_version(client, marathon_app_id, response.json()["version"])
@@ -165,42 +204,77 @@ if __name__ == '__main__':
     stdout_offset = 0
     stderr_offset = 0
     done = False
+    failed = False
+    attempts = 0
 
-    while not done:
-        deployments = client.get_app(marathon_app_id).deployments
+    # Allow up to marathon_retries tasks to fail
+    while attempts < marathon_retries:
 
-        if deployments == []:
-            time.sleep(3)
-            done = True
-        # TODO: The deployment does not get replaced on failure, but rather
-        # restarts and tries again.  So we need to retrieve the task itself,
-        # and check if it has reached TASK_FAILED status
-        # else:
-        #     deployment_found = False
-        #     for deployment in deployments:
-        #         if deployment.id == deployment_id:
-        #             deployment_found = True
-        #
-        #     # If our original deployment no longer exists, then it failed.
-        #     if not deployment_found:
-        #         time.sleep(5)
-        #         done = True
-        #         exit_code = 1
+        # Stream stdout and stderr to the console
+        while not done:
+            deployments = client.get_app(marathon_app_id).deployments
 
-        # time.sleep(0.5)
+            if deployments == []:
+                time.sleep(3)
+                done = True
+            else:
+                mesos_tasks = None
 
-        ### Get STDOUT
+                for host in mesos_master_urls:
+                    response = requests.get(TASK_STATUS_URL.format(host))
+                    #print response.content
+                    if response.status_code == 200:
+                        mesos_tasks = response.json()
+                        #break
 
-        stderr_url = STDERR_URL.format(agent_hostname, new_task.slave_id, framework_id, new_task.id, container_id)
-        stderr_offset = print_file_chunk(stderr_url, stderr_offset, auth)
+                if mesos_tasks:
+                    for task in mesos_tasks['tasks']:
+                        #print task
+                        if task['id'] == new_task.id:
+                            if task['state'] == "TASK_FAILED":
+                                failed = True
+                                done = True
 
-        ### Get STDERR
 
-        stdout_url = STDOUT_URL.format(agent_hostname, new_task.slave_id, framework_id, new_task.id, container_id)
-        stdout_offset = print_file_chunk(stdout_url, stdout_offset, auth)
+            # TODO: The deployment does not get replaced on failure, but rather
+            # restarts and tries again.  So we need to retrieve the task itself,
+            # and check if it has reached TASK_FAILED status
+            # else:
+            #     deployment_found = False
+            #     for deployment in deployments:
+            #         if deployment.id == deployment_id:
+            #             deployment_found = True
+            #
+            #     # If our original deployment no longer exists, then it failed.
+            #     if not deployment_found:
+            #         time.sleep(5)
+            #         done = True
+            #         exit_code = 1
 
-        # Small rate limiting factor
-        time.sleep(0.1)
+            # time.sleep(0.5)
 
-    print "All deployments completed sucessfully!"
+            ### Get STDOUT
+
+            stderr_url = STDERR_URL.format(agent_hostname, new_task.slave_id, framework_id, new_task.id, container_id)
+            stderr_offset = print_file_chunk(stderr_url, stderr_offset, auth)
+
+            ### Get STDERR
+
+            stdout_url = STDOUT_URL.format(agent_hostname, new_task.slave_id, framework_id, new_task.id, container_id)
+            stdout_offset = print_file_chunk(stdout_url, stdout_offset, auth)
+
+            # Small rate limiting factor
+            time.sleep(0.1)
+
+        if failed:
+            attempts += 1
+            done = False
+        else:
+            break
+
+    if failed:
+        print "Failure deploying new app configuration, aborting deployment!"
+        exit_code = 1
+    else:
+        print "All deployments completed sucessfully!"
     sys.exit(exit_code)
